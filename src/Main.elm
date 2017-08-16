9b1779d6b583b749265aaeab35d5f7d746d93a11
Steve Herrin <steve.herrin@gmail.com>
2017-08-15 21:12:36 -0700

    Add transforms and temporary controls

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
@@ -20,12 +21,20 @@ main =
 
 
 type alias Model =
-    {}
+    { scale : Float
+    , rotation : Float
+    , transX : Float
+    , transY : Float
+    }
 
 
 initialModel : Model
 initialModel =
-    {}
+    { scale = 1
+    , rotation = 0
+    , transX = 0
+    , transY = 0
+    }
 
 
 init : ( Model, Cmd Msg )
@@ -61,7 +70,9 @@ nLines =
 
 
 rotate { x, y, z } =
-    -- rotate (x, y, z) such that (1, 1, 1) -> (0, 0, sqrt(3))
+    -- rotate (x, y, z) such that
+    -- (1, 1, 1) -> (0, 0, sqrt(3))
+    -- (1, 1, -1) -> (2*sqrt(2/3), 0, 1/sqrt(3))
     { x = x / sqrt 6 + y / sqrt 6 - 2 * z / sqrt 6
     , y = -x / sqrt 2 + y / sqrt 2
     , z = x / sqrt 3 + y / sqrt 3 + z / sqrt 3
@@ -81,8 +92,9 @@ walls =
             }
 
         wall3 i =
-            { start = rotate { x = 1.0, y = 1.0 - 1.0 / nLines, z = i / nLines }
-            , end = rotate { x = -1.0, y = 1.0 - 1.0 / nLines, z = i / nLines }
+            -- not sure why I need the factor of 2 (TODO)
+            { start = rotate { x = 1.0, y = 1.0 - 1.0 / nLines, z = 1 - 2 * i / nLines }
+            , end = rotate { x = -1.0, y = 1.0 - 1.0 / nLines, z = 1 - 2 * i / nLines }
             }
     in
     [ { color = "#ff0000"
@@ -114,14 +126,42 @@ walls =
 
 
 type Msg
-    = NoOp
+    = ScaleUp
+    | ScaleDown
+    | RotateCW
+    | RotateCCW
+    | TransPlusX
+    | TransMinusX
+    | TransPlusY
+    | TransMinusY
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        ScaleUp ->
+            ( { model | scale = model.scale + 0.1 }, Cmd.none )
+
+        ScaleDown ->
+            ( { model | scale = model.scale - 0.1 }, Cmd.none )
+
+        RotateCW ->
+            ( { model | rotation = model.rotation + 5 }, Cmd.none )
+
+        RotateCCW ->
+            ( { model | rotation = model.rotation - 5 }, Cmd.none )
+
+        TransPlusX ->
+            ( { model | transX = model.transX + 0.05 }, Cmd.none )
+
+        TransMinusX ->
+            ( { model | transX = model.transX - 0.05 }, Cmd.none )
+
+        TransPlusY ->
+            ( { model | transY = model.transY + 0.05 }, Cmd.none )
+
+        TransMinusY ->
+            ( { model | transY = model.transY - 0.05 }, Cmd.none )
 
 
 
@@ -140,6 +180,17 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     let
+        transforms =
+            "scale("
+                ++ toString model.scale
+                ++ "), rotate("
+                ++ toString model.rotation
+                ++ "), translate("
+                ++ toString model.transX
+                ++ " "
+                ++ toString model.transY
+                ++ ")"
+
         l color segment =
             line
                 [ x1 (toString segment.start.x)
@@ -148,6 +199,7 @@ view model =
                 , y2 (toString segment.end.y)
                 , stroke color
                 , strokeWidth "0.01"
+                , transform transforms
                 ]
                 []
 
@@ -161,4 +213,19 @@ view model =
             , Svg.Attributes.style "background-color: black"
             ]
             (List.concat (List.map segments walls))
+        , div []
+            [ button [ onClick ScaleDown ] [ Html.text "Scale-" ]
+            , button [ onClick ScaleUp ] [ Html.text "Scale+" ]
+            , button [ onClick RotateCW ] [ Html.text "CW" ]
+            , button [ onClick RotateCCW ] [ Html.text "CCW" ]
+            , button [ onClick TransPlusX ] [ Html.text "X+" ]
+            , button [ onClick TransMinusX ] [ Html.text "X-" ]
+            , button [ onClick TransPlusY ] [ Html.text "Y+" ]
+            , button [ onClick TransMinusY ] [ Html.text "Y-" ]
+            ]
+        , div []
+            [ Html.text ("Scale: " ++ toString model.scale)
+            , Html.text ("Rot: " ++ toString model.rotation)
+            , Html.text ("Trans: (" ++ toString model.transX ++ " , " ++ toString model.transY ++ ")")
+            ]
         ]
