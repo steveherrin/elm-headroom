b5b68be27b21162411189e323b8a80a4b6aeb54b
Steve Herrin <steve.herrin@gmail.com>
2017-08-15 20:12:07 -0700

    Draw some walls

--- src/Main.elm
+++ src/Main.elm
@@ -34,6 +34,82 @@ init =
 
 
 
+-- CONSTANTS
+
+
+type alias Point =
+    { x : Float
+    , y : Float
+    , z : Float
+    }
+
+
+type alias Segment =
+    { start : Point
+    , end_ : Point
+    }
+
+
+type alias Wall =
+    { color : String
+    , segments : List Segment
+    }
+
+
+nLines =
+    16
+
+
+rotate { x, y, z } =
+    -- rotate (x, y, z) such that (1, 1, 1) -> (0, 0, sqrt(3))
+    { x = x / sqrt 6 + y / sqrt 6 - 2 * z / sqrt 6
+    , y = -x / sqrt 2 + y / sqrt 2
+    , z = x / sqrt 3 + y / sqrt 3 + z / sqrt 3
+    }
+
+
+walls =
+    let
+        wall1 i =
+            { start = rotate { x = 1.0, y = i / nLines, z = 1.0 }
+            , end = rotate { x = -1.0, y = i / nLines, z = 1.0 }
+            }
+
+        wall2 i =
+            { start = rotate { x = 1.0, y = i / nLines, z = 1.0 }
+            , end = rotate { x = 1.0, y = i / nLines, z = -1.0 }
+            }
+
+        wall3 i =
+            { start = rotate { x = 1.0, y = 1.0 - 1.0 / nLines, z = i / nLines }
+            , end = rotate { x = -1.0, y = 1.0 - 1.0 / nLines, z = i / nLines }
+            }
+    in
+    [ { color = "#ff0000"
+      , segments =
+            List.map wall1
+                (List.map toFloat <|
+                    List.range 0 (nLines - 1)
+                )
+      }
+    , { color = "#00ff00"
+      , segments =
+            List.map wall2
+                (List.map toFloat <|
+                    List.range 0 (nLines - 1)
+                )
+      }
+    , { color = "#0000ff"
+      , segments =
+            List.map wall3
+                (List.map toFloat <|
+                    List.range 0 (nLines - 1)
+                )
+      }
+    ]
+
+
+
 -- UPDATE
 
 
@@ -63,8 +139,26 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
+    let
+        l color segment =
+            line
+                [ x1 (toString segment.start.x)
+                , x2 (toString segment.end.x)
+                , y1 (toString segment.start.y)
+                , y2 (toString segment.end.y)
+                , stroke color
+                , strokeWidth "0.01"
+                ]
+                []
+
+        segments w =
+            List.map (l w.color) w.segments
+    in
     div []
-        [ svg [ viewBox "-1 -1 2 2", width "600px" ]
-            [ circle [ cx "0", cy "0", r "0.1", fill "#cccccc" ] []
+        [ svg
+            [ viewBox "-0.65 -0.65 1.3 1.3"
+            , width "600px"
+            , Svg.Attributes.style "background-color: black"
             ]
+            (List.concat (List.map segments walls))
         ]
