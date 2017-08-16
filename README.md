7b0bf222bfa977f7fb4519422fdb19c702d463ea
Steve Herrin <steve.herrin@gmail.com>
2017-08-15 19:08:00 -0700

    Set up basic project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Elm Headroom
+
+Experiment using SVG graphics to create a Max Headroom -like background
---
+++ src/Main.elm
@@ -0,0 +1,70 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+initialModel : Model
+initialModel =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ svg [ viewBox "-1 -1 2 2", width "600px" ]
+            [ circle [ cx "0", cy "0", r "0.1", fill "#cccccc" ] []
+            ]
+        ]
